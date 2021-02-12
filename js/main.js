c54bb80f39516029e5777838e437e6f3e8c8f458
cheon <cheonjp87@gmail.com>
2021-02-13 03:21:03 +0900

    Edition

--- js/main.js
+++ js/main.js
@@ -84,4 +84,92 @@
         })
     }, 5000)
 
+})();
+
+(function () {
+    const itemName = document.querySelector('.item-name')
+    const itemText = document.querySelector('.item-text')
+    const itemPrice = document.querySelector('#price')
+    const dots = document.querySelectorAll('.slide-circle')
+    const itemImg = document.querySelector('.item-img')
+    const play = document.querySelector('.play')
+    const pause = document.querySelector('.pause')
+    let index = 0
+    const items = [
+        {
+            name: 'Cheese Bomb', text: '모짜렐라 치즈가 두배 더 들어간 애니타임의 스페셜 피자로 가격과 맛 두가지를 모두 잡은 피자',
+            price: '28,000원', img: '../img/intro-1.jpg'
+        },
+        {
+            name: 'Double Spicy Lucola', text: '이태리 핫소스와 토마토소스, 루꼴라 함께 어우러져 매운맛을 사랑하는 사람을 위한 피자',
+            price: '27,000원', img: '../img/intro-2.jpg'
+        },
+        {
+            name: 'Pazla', text: '햄이 더욱 듬뿍 그리고 스페셜 갈릭 소스와 바베큐 소스가 만나 더욱 스페셜 한 맛의 피자',
+            price: '27,000원', img: '../img/intro-3.jpg'
+        }
+    ]
+
+    const infinite = setInterval(function () {
+        index++
+        if (index > dots.length - 1) {
+            index = 0
+        }
+        itemImg.classList.add('active')
+        setTimeout(function () {
+            itemImg.classList.remove('active')
+        }, 2000)
+        let i
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = 'slide-circle'
+            dots[index].className = 'circle-active'
+            itemName.textContent = items[index].name
+            itemText.textContent = items[index].text
+            itemPrice.textContent = items[index].price
+            itemImg.src = items[index].img
+        }
+    }, 5000)
+
+    clickChange()
+    function clickChange() {
+        let i
+        for (i = 0; i < dots.length; i++) {
+            dots[i].addEventListener('click', function (e) {
+                e.target.className = 'circle-active'
+                if (e.target === dots[0]) {
+                    index = 0
+                    dots[1].className = 'slide-circle'
+                    dots[2].className = 'slide-circle'
+                } else if (e.target === dots[1]) {
+                    index = 1
+                    dots[0].className = 'slide-circle'
+                    dots[2].className = 'slide-circle'
+                } else if (e.target === dots[2]) {
+                    index = 2
+                    dots[0].className = 'slide-circle'
+                    dots[1].className = 'slide-circle'
+                }
+
+                console.log(index)
+                console.log(e.target)
+                itemName.textContent = items[index].name
+                itemText.textContent = items[index].text
+                itemPrice.textContent = items[index].price
+                itemImg.src = items[index].img
+            })
+        }
+    }
+
+    play.addEventListener('click', function (e) {
+        e.target.style.display = 'none'
+        pause.style.display = 'block'
+    })
+
+    pause.addEventListener('click', function (e) {
+        clearInterval(infinite)
+        clickChange()
+        e.target.style.display = 'none'
+        play.style.display = 'block'
+    })
+
 })()
